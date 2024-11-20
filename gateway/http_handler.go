cddb672365f9bb6a7bfce7e0a0a1ba3412a23e04
muhammad andre widianto <andrewidianto@gmail.com>
2024-11-20 22:38:02 +0700

    add http handler

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/andrewidianto/common"
+	pb "github.com/andrewidianto/common/api"
 )
 
 type handler struct {
 	//gateway
+	client pb.OrderServiceClient
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(client pb.OrderServiceClient) *handler {
+	return &handler{client}
 }
 
 func (h *handler) registerRoutes(mux *http.ServeMux) {
@@ -17,4 +21,16 @@ func (h *handler) registerRoutes(mux *http.ServeMux) {
 }
 
 func (h *handler) HandleCreateOrder(w http.ResponseWriter, r *http.Request) {
+	customerID := r.PathValue("customerID")
+
+	var items []*pb.ItemsWithQuantity
+	if err := common.ReadJson(r, &items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+		CustomerID: customerID,
+		Items:      items,
+	})
 }
